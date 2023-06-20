8c043ca444831a4fd085bc4acf3962f88ef888b9
angel <angelwitchell2001@gmail.com>
2023-06-20 18:08:52 +0100

    Using a different Weather API.

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,54 +1,64 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Weather.css";
 const Weather = () => {
-  const [location, setLocation] = useState("Location");
-  const [weather, setWeather] = useState("Weather");
+  const [location, setLocation] = useState("");
+  const [weather, setWeather] = useState("");
+  const [weatherDescription, setWeatherDescription] = useState("");
   const [weatherIcon, setWeatherIcon] = useState("");
-  const [temperatureC, setTemperatureC] = useState(0);
-  const [temperatureF, setTemperatureF] = useState(0);
+  const [temperature, setTemperature] = useState("");
+  const [temperatureF, setTemperatureF] = useState("");
+
   const [data, setdata] = useState(null);
+  const API_KEY = "6bc0cc514753cf1e36f040a0a5cf858b";
 
-  const enterHandler = (e) => {
-    if (e.key === "Enter") {
-      setLocation(e.target.value);
-      getData();
-      e.target.value = null;
-    }
-  };
+  const inputHandler = (e) => setLocation(e.target.value);
 
-  const getData = () => {
-    if (location != null) {
-      fetch(
-        "https://api.weatherapi.com/v1/forecast.json?key=573ced0133c744eb9a1142555230506&q=" +
-          location
-      )
-        .then((response) => response.json())
-        .then((json) => setdata(json))
-        .catch((error) => console.error(error));
-    }
+  const getData = async () => {
+    if (location != "") {
+      const response = await fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          location +
+          "&appid=" +
+          API_KEY +
+          "&units=metric"
+      );
 
-    if (data != null) {
-      setTemperatureC(data["current"]["temp_c"]);
-      setTemperatureF(data["current"]["temp_f"]);
-      setWeather(data["current"]["condition"]["text"]);
-      setWeatherIcon(data["current"]["condition"]["icon"]);
+      setdata(await response.json());
+
+      if (data != []) {
+        setTemperature(data["main"]["temp"] + "C");
+        setTemperatureF((data["main"]["temp"] * (9 / 5) + 32).toFixed(2) + "F");
+
+        for (let i of data["weather"]) {
+          setWeatherIcon(
+            "http://openweathermap.org/img/w/" + i["icon"] + ".png"
+          );
+          setWeather(i["main"]);
+          setWeatherDescription(i["description"]);
+        }
+      } else {
+        return;
+      }
     }
   };
 
   return (
     <div className='weatherContainer'>
       <input
-        type='search'
+        type='text'
+        value={location}
+        onChange={inputHandler}
         placeholder='Type your location here'
-        onKeyDown={enterHandler}
       />
+      <button onClick={getData}>→</button>
       <div className='weatherCol'>
         <h1>{location}</h1>
+        <img src={weatherIcon}></img>
         <div className='weatherCol'>
-          <img src={weatherIcon}></img>
           <h3>{weather}</h3>
-          <h3>{temperatureC}C</h3>
-          <h3>{temperatureF}F</h3>
+          <h3>{weatherDescription}</h3>
+          <h3>{temperature}</h3>
+          <h3>{temperatureF}</h3>
         </div>
       </div>
     </div>
